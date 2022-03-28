87a7eca84a059ad8b38150b58e288c241e2e249a
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-28 11:06:36 -0300

    1º commit branch testing

--- ex77.py
+++ ex77.py
@@ -1,15 +1,2 @@
-#77
-palavras = (
-    'APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON',
-    'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR',
-    'MERCADO', 'PROGRAMADOR', 'FUTURO', 'ZHS',
-)
-
-vogais = ('A', 'E', 'I', 'O', 'U')
-
-for palavra in palavras:
-    print(f"\nNa palavra {palavra} temos ", end="")
-    for letra in palavra:
-        if letra in vogais:
-            print(letra.lower(), end=" ")
-        
+#test branch testing
+    
