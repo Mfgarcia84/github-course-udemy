043847676084052d0a48275422477c9a4ae3e117
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2022-03-29 14:43:13 -0300

    1º commit atualização repositório github

---
+++ ex090.py
@@ -0,0 +1,16 @@
+# Exercício 90
+
+aluno = {}
+aluno['nome'] = str(input("Nome: "))
+aluno['media'] = float(input(f"Média de {aluno['nome']}: "))
+if aluno['media'] < 5:
+    situacao = "Reprovado"
+elif aluno['media'] < 7:
+    situacao = "Recuperação"
+else:
+    situacao = "Aprovado"
+aluno['situação'] = situacao
+
+print("-="*40)
+for k, v in aluno.items():
+    print(f"{k} é igual a {v}")
--- ex091.py
+++ ex091.py
@@ -1,2 +1,24 @@
-#mudança no branch main
-#mudança no branch develop
+# Exercício 91
+
+from multiprocessing.sharedctypes import Value
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogos = {}
+for sorteio in range(1, 5):
+    numero = randint(1, 6)
+    jogos[f'Jogador:{sorteio}'] = numero
+print(f"\n{jogos}")
+for jogador, valor in jogos.items():
+    print(f"{jogador} tirou {valor} no dado.")
+    sleep(1)
+print("-"*26)
+print(f"{'RANKING DOS JOGADORES':^26}")
+print("-"*26)
+
+ranking = {}
+ranking = sorted(jogos.items(), key=itemgetter(1), reverse=True)
+print(ranking)
+
+for i, v in enumerate(ranking):
+    print(f"{i+1}º Lugar: {v[0]} com {v[1]} pontos.")
